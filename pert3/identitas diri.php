1298b80b18e8f7e27de9d40fc8be43a874cbf38c
heraamalia <1904030088@students.unis.ac.id>
2022-01-22 18:21:18 +0700

    pertemuan 3

---
+++ pert3/identitas diri.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>identitas</title>
+    </head>
+<body>
+<?php
+$namadep= "Hera";
+$namateng= "Amalia";
+$namabel= "Agustin";
+$mk= "Web Programing";
+$nim= "1904030088";
+$ttl= "Tangerang 08 Agustus 2001";
+$jeniskel= "Perempuan";
+$usia= "(0x14)";
+$hobi= "Tidur";
+$alm= "Tigaraksa";
+$telp= "0812xxxxxxx";
+?>
+
+<h4>Identitas Diri</h4>
+<p>Nama : <?php echo $namadep ." ".$namateng ." ".$namabel;?></p>
+<p>Mata Kuliah : <?php echo $mk;?></p>
+<p>NIM : <?php echo $nim;?></p>
+<p>Tempat, Tanggal Lahir : <?php echo $ttl;?></p>
+<p>Jenis Kelamin : <?php echo $jeniskel;?></p>
+<p>Usia : <?php echo $usia;?></p>
+<p>Hobi : <?php echo $hobi;?></p>
+<p>Alamat : <?php echo $alm;?></p>
+<p>No. Telpon : <?php echo $telp;?></p>
+</body>
+</html>
